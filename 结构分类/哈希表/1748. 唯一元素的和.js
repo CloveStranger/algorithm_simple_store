73a7b39e72103905d2449a1e4a5b84010126b0df
Ziang Zhu <ziang_zhu@thinkyeah.com>
2023-12-01 09:38:01 +0800

    update

---
+++ 结构分类/哈希表/1748. 唯一元素的和.js
@@ -0,0 +1,42 @@
+// 给你一个整数数组 nums 。数组中唯一元素是那些只出现 恰好一次 的元素。
+
+// 请你返回 nums 中唯一元素的 和 。
+
+// 示例 1：
+
+// 输入：nums = [1,2,3,2]
+// 输出：4
+// 解释：唯一元素为 [1,3] ，和为 4 。
+// 示例 2：
+
+// 输入：nums = [1,1,1,1,1]
+// 输出：0
+// 解释：没有唯一元素，和为 0 。
+// 示例 3 ：
+
+// 输入：nums = [1,2,3,4,5]
+// 输出：15
+// 解释：唯一元素为 [1,2,3,4,5] ，和为 15 。
+
+// 提示：
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function (nums) {
+  const map = new Map();
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+  }
+  for (let [key, value] of map) {
+    if (value === 1) {
+      sum += key;
+    }
+  }
+
+  return sum;
+};
