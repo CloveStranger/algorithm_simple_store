d340a2ba4431eb3b44d2aa011d53d6b6b6cc0157
Ziang Zhu <ziang_zhu@thinkyeah.com>
2024-01-15 09:32:24 +0800

    update

---
+++ 结构分类/数组/middle/46. 全排列.js
@@ -0,0 +1,44 @@
+// 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+
+// 示例 1：
+
+// 输入：nums = [1,2,3]
+// 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// 示例 2：
+
+// 输入：nums = [0,1]
+// 输出：[[0,1],[1,0]]
+// 示例 3：
+
+// 输入：nums = [1]
+// 输出：[[1]]
+
+// 提示：
+
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// nums 中的所有整数 互不相同
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let res = [];
+  let dfs = (path) => {
+    console.log(path);
+    if (path.length === nums.length) {
+      res.push(path);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (path.includes(nums[i])) continue;
+      path.push(nums[i]);
+      dfs(path.slice());
+      path.pop();
+    }
+  };
+  dfs([]);
+  return res;
+};
+nums = [1, 2, 3];
+console.log(permute(nums));
