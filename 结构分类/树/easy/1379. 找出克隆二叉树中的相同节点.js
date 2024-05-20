70265a97961f5e1a0db14e7fa6c14a38e7918af6
dcdev_mini <dcdev_flutter@dcdev-minideMac-mini.local>
2024-05-20 09:55:21 +0800

    update

--- 结构分类/树/easy/1379. 找出克隆二叉树中的相同节点.js
+++ 结构分类/树/easy/1379. 找出克隆二叉树中的相同节点.js
@@ -46,33 +46,59 @@ function TreeNode(val) {
   this.left = this.right = null;
 }
 
-var getTargetCopy = function (original, cloned, target) {
-  if (original.val === target) {
-    return cloned;
-  }
-  const dp = (root) => {
-    if (root === null) return null;
-    if (root.val === target) return root;
-    const left = dp(root.left);
-    const right = dp(root.right);
-    if (left !== null) return left;
-    if (right !== null) return right;
-    return null;
-  };
+// var getTargetCopy = function (original, cloned, target) {
+//   if (original.val === target) {
+//     return cloned;
+//   }
+//   const dp = (root) => {
+//     if (root === null) return null;
+//     if (root.val === target) return root;
+//     const left = dp(root.left);
+//     const right = dp(root.right);
+//     if (left !== null) return left;
+//     if (right !== null) return right;
+//     return null;
+//   };
 
-  return dp(original);
-};
+//   return dp(original);
+// };
+
+// var getTargetCopy = function (original, cloned, target) {
+//   if (original == null) {
+//     return null;
+//   }
+//   if (original == target) {
+//     return cloned;
+//   }
+//   const left = getTargetCopy(original.left, cloned.left, target);
+//   if (left != null) {
+//     return left;
+//   }
+//   return getTargetCopy(original.right, cloned.right, target);
+// };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
 
 var getTargetCopy = function (original, cloned, target) {
-  if (original == null) {
-    return null;
-  }
-  if (original == target) {
+  if (original === null) return null;
+  if (original === target) {
     return cloned;
   }
   const left = getTargetCopy(original.left, cloned.left, target);
-  if (left != null) {
-    return left;
+  if (left !== null) {
+    returnleft;
   }
   return getTargetCopy(original.right, cloned.right, target);
 };
